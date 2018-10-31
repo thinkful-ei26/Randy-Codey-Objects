0b6e3d32d17eba76b1a7c2342ef734541266d9bb
Codey Worley <codeyworley@gmail.com>
2018-10-31 08:56:46 -0700

    finished first problem

---
+++ objects-1.js
@@ -0,0 +1,11 @@
+const createMyObject = () => {
+
+    return {
+        foo: 'bar',
+        answerToUniverse: 42,
+        'olly olly': 'oxen free',
+        sayHello: () => 'hello'
+    }
+}
+
+console.log(createMyObject());
