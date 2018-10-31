66cfccc2fd152a14d60e59747c772f8ce916bb37
randy sartor <randal.sartor@gmail.com>
2018-10-31 14:41:09 -0400

    exercise 3

--- object-drills.js
+++ object-drills.js
@@ -34,3 +34,15 @@ for (const prop in object1) {
     console.log(`${prop}: ${object1[prop]}`)
 }
 
+
+
+// Create an object with a property called meals which is an array 
+// of strings: 'breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'.
+// Use console.log to show the name of a hobbit's fourth meal of the day.
+// Don't forget that humans and hobbits count from 1, but computers count from 0.
+
+const food = {
+    meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'],
+};
+
+console.log(food.meals[3])
