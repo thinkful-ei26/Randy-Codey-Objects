652c9e3f25bfa80a5f3101154fc5db60dae947f7
Codey Worley <codeyworley@gmail.com>
2018-10-31 09:15:24 -0700

    finished problems

--- objects-1.js
+++ objects-1.js
@@ -8,4 +8,48 @@ const createMyObject = () => {
     }
 }
 
-console.log(createMyObject());
+const newObj = createMyObject();
+
+
+const updateObject = (obj) => {
+    obj.foo = 'foo';
+    obj.bar = 'bar';
+    obj.bizz = 'bizz';
+    obj.bang = 'bang';
+
+    return obj;
+}
+
+updateObject(newObj);
+
+
+function personMaker() {
+    var person = {
+      firstName: 'Paul',
+      lastName: 'Jones',
+      // replace `null` with a function that uses self reference to return
+      // full name
+      fullName: function() { return `${this.firstName} ${this.lastName}`},
+    };
+    return person;
+  }
+
+  const person = personMaker();
+
+
+const sampleObj = {
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang',
+    };
+
+    function keyDeleter(obj) {
+    // your code here
+    delete obj.foo;
+    delete obj.bar;
+
+    return obj;
+}
+
+console.log(keyDeleter(sampleObj));
